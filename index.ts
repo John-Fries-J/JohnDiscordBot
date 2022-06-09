512f31a7f798e4dd03aae01d1571778fcba041ab
John-Fries-J <w.bababooie@gmail.com>
2022-06-09 22:48:33 +0100

    new stuff

--- index.ts
+++ index.ts
@@ -7,7 +7,9 @@ dotenv.config()
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
   ]
 })
 
