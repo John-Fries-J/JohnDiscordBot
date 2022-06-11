a08df2817df0277d2a571e7b76872e10c025d8bc
John-Fries-J <w.bababooie@gmail.com>
2022-06-11 23:31:54 +0100

    Update status.ts

--- Commands/status.ts
+++ Commands/status.ts
@@ -1,25 +1,41 @@
-import { MessageEmbed } from "discord.js";
-import { ICommand } from "wokcommands";
+import { Client } from 'discord.js'
+import { ICommand } from 'wokcommands'
+
+const setStatus = (client: Client, status: string) => {
+  client.user?.setPresence({
+    status: 'online',
+    activities: [
+      {
+        name: status,
+      },
+    ],
+  })
+}
 
 export default {
-    category: 'Configuration',
-    description: 'Sets bot status',
-    permissions: ['ADMINISTRATOR'],
+  category: 'Configuration',
+  description: 'Updates the status for the bot',
+  
 
-    minArgs: 1,
-    expectedArgs: '<status>',
-    ownerOnly: true,
+  minArgs: 1,
+  expectedArgs: '<status>',
+  
 
-    slash: 'both',
+  ownerOnly: true,
+  
 
-    callback:  ({ client, text }) => {
-        client.user?.setPresence({
-            status: 'dnd',
-            activities: [
-                {
-                    name: text
-                }
-            ]
-        })
-    },
+  init: (client: Client) => {
+
+    const status = "Testing" 
+    setStatus(client, status)
+  },
+  
+
+  callback: ({ client, text, message }) => {
+    setStatus(client, text)
+
+    message.reply({
+      content: 'Status set!'
+    })
+  },
 } as ICommand
