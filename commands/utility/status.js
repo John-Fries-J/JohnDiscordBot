beca0b9f50d6c9b871bd9c4beee1b3022c2713fb
Nishant1500 <53296643+Nishant1500@users.noreply.github.com>
2022-06-13 12:31:03 +0530

    Update status.js

--- commands/utility/status.js
+++ commands/utility/status.js
@@ -16,7 +16,9 @@ module.exports = {
 	 */
 
 	async execute(client, message, args) {
-         if(!admins.includes(message.author.id)) return message.reply({ content: 'You don't have permission to use this.' })
+         if(!admins.includes(message.author.id)) {
+return message.reply({ content: 'You don't have permission to use this.' })
+}
           
 // Set the client user's presence
 client.user.setPresence({ activities: [{ name: `${args[0]` }], status: args[1] ? args[1] : client.user.presence.status });
