23d682b774a7bd838e70523e6ea0491a6da69075
Nishant1500 <53296643+Nishant1500@users.noreply.github.com>
2022-06-13 11:31:47 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -8,11 +8,170 @@ module.exports = {
 	 */
 
 	async execute(message, client) {
-     if(message.content.toLowerCase().includes(`<@${client.user.id}>`)) {
-       message.reply({
-         content: 'Ayo? this works?'
 
-       })
-     }
+		// Declares const to be used.
+
+		const { client, guild, channel, content, author } = message;
+
+		// Checks if the bot is mentioned in the message all alone and triggers onMention trigger.
+		// You can change the behavior as per your liking at ./messages/onMention.js
+
+		if (
+			message.content == `<@${client.user.id}>` ||
+			message.content == `<@!${client.user.id}>`
+		) {
+require('../utils/onMention.js').execute(message)
+return;
+		}
+
+		/**
+		 * @description Converts prefix to lowercase.
+		 * @type {String}
+		 */
+const prefix = '!';
+
+		const checkPrefix = prefix.toLowerCase();
+
+		/**
+		 * @description Regex expression for mention prefix
+		 */
+
+		const prefixRegex = new RegExp(
+			`^(<@!?${client.user.id}>|${escapeRegex(checkPrefix)})\\s*`
+		);
+
+		// Checks if message content in lower case starts with bot's mention.
+
+		if (!prefixRegex.test(content.toLowerCase())) return;
+
+		/**
+		 * @description Checks and returned matched prefix, either mention or prefix in config.
+		 */
+
+		const [matchedPrefix] = content.toLowerCase().match(prefixRegex);
+
+		/**
+		 * @type {String[]}
+		 * @description The Message Content of the received message seperated by spaces (' ') in an array, this excludes prefix and command/alias itself.
+		 */
+
+		const args = content.slice(matchedPrefix.length).trim().split(/ +/);
+
+		/**
+		 * @type {String}
+		 * @description Name of the command received from first argument of the args array.
+		 */
+
+		const commandName = args.shift().toLowerCase();
+
+		// Check if mesage does not starts with prefix, or message author is bot. If yes, return.
+
+		if (!message.content.startsWith(matchedPrefix) || message.author.bot)
+			return;
+
+		/**
+		 * @description The message command object.
+		 * @type {Object}
+		 */
+
+		const command =
+			client.commands.get(commandName) ||
+			client.commands.find(
+				(cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+			);
+
+		// It it's not a command, return :)
+if(!command && message.content.toLowerCase().includes(`<@${client.user.id}>`)) {
+require('../utils/onMention.js').execute(message)
+return;
+}
+		if (!command) return;
+
+		// Owner Only Property, add in your command properties if true.
+
+		if (command.adminsOnly && !admins.includes(message.author.id)) {
+			return message.reply({ content: "This is a owner only command!" });
+		}
+
+		// Guild Only Property, add in your command properties if true.
+
+		if (command.guildOnly && message.channel.type === "dm") {
+			return message.reply({
+				content: "I can't execute that command inside DMs!",
+			});
+		}
+
+		// Author perms property
+
+		if (command.permissions) {
+			const authorPerms = message.channel.permissionsFor(message.author);
+			if (!authorPerms || !authorPerms.has(command.permissions)) {
+				return message.reply({ content: "You can not do this!" });
+			}
+		}
+
+		// Args missing
+
+		if (command.args && !args.length) {
+			let reply = `You didn't provide any arguments, ${message.author}!`;
+
+			if (command.usage) {
+				reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+			}
+
+			return message.channel.send({ content: reply });
+		}
+
+		// Cooldowns
+
+		const { cooldowns } = client;
+
+		if (!cooldowns.has(command.name)) {
+			cooldowns.set(command.name, new Collection());
+		}
+
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.name);
+		const cooldownAmount = (command.cooldown || 3) * 1000;
+
+		if (timestamps.has(message.author.id)) {
+			const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+			if (now < expirationTime) {
+				const timeLeft = (expirationTime - now) / 1000;
+				return message.reply({
+					content: `Please wait **${timeLeft.toFixed(
+						1
+					)}** more second(s) before reusing the \`${command.name}\` command.`,
+				});
+			}
+		}
+
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+		// Rest your creativity is below.
+
+		// execute the final command. Put everything above this.
+		try {
+			const sentMessage = await command.execute(client, message, args);
+    await sentMessage?.components.forEach(async (c) => {
+      if(c) {
+          await c.components.forEach(cs => cs.setDisabled(true))
+        await setTimeout(async () => {
+          await sentMessage.edit({ components: sentMessage.components
+                               })
+        }, 15000)
+      }
+    })
+      
+console.log(`${message.guild.name}:${message.guild.id} | ${message.author.tag} Used ${commandName}`)
+		} catch (error) {
+			console.log('Error: \n' + chalk.red(error));
+			message.reply({
+				content: "There was an error trying to execute that command!",
+			});
+		}
+
   }
 }
